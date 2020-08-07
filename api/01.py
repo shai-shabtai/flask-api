0792b9c32e6519d318f9db9418b2f71c34da2343
shai-shabtai <shai.shabtai15@gmail.com>
2020-08-07 09:26:10 +0000

    upload from server

---
+++ api/01.py
@@ -0,0 +1,36 @@
+import json
+import os
+from flask import Flask, Response, abort
+from .utils import JSON_MIME_TYPE, search_book
+#API_USER = os.getenv('SECRETS_API_USER')
+BOOK_TITLE = os.getenv('BOOK_TITLE')
+app = Flask(__name__)
+
+books = [{
+    'id': 33,
+    'title': 'The Raven',
+    'author_id': 1
+}]
+
+
+@app.route('/book')
+def book_list():
+    response = Response(
+        json.dumps(books), status=200, mimetype=JSON_MIME_TYPE)
+    return response
+
+
+@app.route('/book/<int:book_id>')
+def book_detail(book_id):
+    book = search_book(books, book_id)
+    book['title'] = BOOK_TITLE
+    if book is None:
+        abort(404)
+
+    content = json.dumps(book)
+    return content, 200, {'Content-Type': JSON_MIME_TYPE}
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return '', 404
